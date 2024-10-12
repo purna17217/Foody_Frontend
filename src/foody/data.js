06ac5daf44f54e36075b120ad8b1613ec2919112
purna17217 <tirunagaripurnachandrarao@gmail.com>
2024-10-13 00:35:07 +0530

    Data

---
+++ src/foody/data.js
@@ -0,0 +1,31 @@
+import Bonda from '/assets/item/Bonda.jpg';
+import Dosa from '/assets/item/Dosa.jpg';
+import Idli from '/assets/item/Idli.jpg';
+import Juice from '/assets/item/Juice.jpg';
+import Pancake from '/assets/item/Pancake.jpg';
+import Paratha from '/assets/item/Paratha.jpg';
+import Poha from '/assets/item/Poha.jpg';
+import Poori from '/assets/item/Poori.jpg';
+import Vada from '/assets/item/Vada.jpg';
+import Pastry from '/assets/item/Pastry.jpg';
+import Salad from '/assets/item/Salad.jpg';
+import Burger from '/assets/item/Burger.jpg';
+import Biriyaani from '/assets/item/Biriyaani.jpg';
+import Pizza from '/assets/item/Pizza.jpg';
+
+export const itemData = [
+  { item_img: Biriyaani },
+  { item_img: Bonda },
+  { item_img: Burger },
+  { item_img: Dosa },
+  { item_img: Idli },
+  { item_img: Juice },
+  { item_img: Pancake },
+  { item_img: Paratha },
+  { item_img: Pastry },
+  { item_img: Pizza },
+  { item_img: Poha },
+  { item_img: Poori },
+  { item_img: Salad },
+  { item_img: Vada },
+];
